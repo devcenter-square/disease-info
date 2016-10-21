ddd5fd361e6dc6d23b8e3afd74f053165aac8646
luizvarela <luiz.lvarela@gmail.com>
2016-10-20 23:30:46 -0300

    Create initial scraper tests using VCR gem

--- spec/lib/scrapers/who_scraper_spec.rb
+++ spec/lib/scrapers/who_scraper_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 describe Scrapers::WhoScraper do
-  def disease_mock
-
+  def get_data
+    VCR.use_cassette('who/scrapers') do
+      Scrapers::WhoScraper.get_data
+    end
   end
 
-  let (:scrape_klass) { Scrapers::WhoScraper }
-
-  describe "#get_data" do
-    it "should save gotten disease to the database" do
-      skip "should test scraper?"
-      expect(scrape_klass).to receive(:collect_data).and_return(disease_mock)
-      scrape_klass.get_data
+  describe '#get_data' do
+    it 'should save gotten disease to the database' do
+      expect { get_data }.to change { Disease.count }
     end
   end
 end
--- spec/support/webmock.rb
+++ spec/support/webmock.rb
@@ -1,13 +1,13 @@
 require 'webmock/rspec'
 
-def body_mock
-  ["disease"]
-end
-
-RSpec.configure do |config|
-  config.before do
-    stub_request(:get, "http://www.who.int/topics/infectious_diseases/factsheets/en/").
-      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'ruby'}).
-      to_return(:status => 200, :body => body_mock, :headers => {})
-  end
-end
+# def body_mock
+#   ["disease"]
+# end
+#
+# RSpec.configure do |config|
+#   config.before do
+#     stub_request(:get, "http://www.who.int/topics/infectious_diseases/factsheets/en/").
+#       with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'ruby'}).
+#       to_return(:status => 200, :body => body_mock, :headers => {})
+#   end
+# end
