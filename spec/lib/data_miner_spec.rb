d799567d94864d27ab2d9880e0d3113f706c1227
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2016-11-02 17:22:01 +0100

    .....,

--- spec/lib/data_miner_spec.rb
+++ spec/lib/data_miner_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe DataMiner do
   describe "#get_who_data" do
-    it "should call the #data method of Scrapers::Who::DiseaseParser" do
-      allow(Scrapers::Who::DiseaseParser).to receive(:data)
+    it "should call the #perform method of Scrapers::Who::Parser" do
+      allow(Scrapers::Who::Parser).to receive(:perform)
       DataMiner.get_who_data
     end
   end
