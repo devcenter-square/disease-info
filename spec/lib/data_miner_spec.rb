e364a0fce560fb86c7e0f4ce306c0db4d8cf3cd8
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2016-11-02 13:28:46 +0100

    Fixed data_miner_spec.rb to get the refactored :data methos from DiseaseParser
    get_who_data should call the #data method of Scrapers::Who::DiseaseParser instead of the deleted :get_data method in WhoScraper

--- spec/lib/data_miner_spec.rb
+++ spec/lib/data_miner_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe DataMiner do
   describe "#get_who_data" do
-    it "should call the #get_data method of Scrapers::Who::DiseaseParser" do
-      expect(Scrapers::Who::DiseaseParser).to receive(:get_data)
+    it "should call the #data method of Scrapers::Who::DiseaseParser" do
+      expect(Scrapers::Who::DiseaseParser).to receive(:data)
       DataMiner.get_who_data
     end
   end
