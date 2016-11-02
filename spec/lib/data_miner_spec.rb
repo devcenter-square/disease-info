87f17e381b7ee65f5e21bef6bc83fd19c1ef59a8
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2016-11-02 12:37:30 +0100

    Fixed failing test
    Travis shows the class is the reason the PR fails test, and its refactored.

--- spec/lib/data_miner_spec.rb
+++ spec/lib/data_miner_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe DataMiner do
   describe "#get_who_data" do
-    it "should call the #get_data method of Scrapers::WhoScraper" do
-      expect(Scrapers::WhoScraper).to receive(:get_data)
+    it "should call the #get_data method of Scrapers::Who::DiseaseParser" do
+      expect(Scrapers::Who::DiseaseParser).to receive(:get_data)
       DataMiner.get_who_data
     end
   end
