21debecb34d6bc1402450eb3b36672d75cc00905
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2016-11-02 13:44:44 +0100

    mock RSpec straightforward

--- spec/lib/data_miner_spec.rb
+++ spec/lib/data_miner_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe DataMiner do
   describe "#get_who_data" do
     it "should call the #data method of Scrapers::Who::DiseaseParser" do
-      expect(Scrapers::Who::DiseaseParser).to receive(:data)
+      allow(Scrapers::Who::DiseaseParser).to receive(:data)
       DataMiner.get_who_data
     end
   end
