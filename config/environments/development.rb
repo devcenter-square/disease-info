b870944639bdc62909f9cb71e51511f80ff25aa9
Afolabi Olaoluwa <contact@afolabiolaoluwa.com>
2016-11-08 12:44:45 +0100

    Edited development.rb
    Pushed configuration for handling 404,500, etc right to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,10 +12,8 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  # To see your normal errors with trace, then uncomment this
-  #config.consider_all_requests_local = true
-  
-  # Disable caching.
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
