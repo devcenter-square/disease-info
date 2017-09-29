ccda6fa6f4478092c4fa7b027ba800a06f2aac39
GabrielSVinha <gabriel.vinha@ccc.ufcg.edu.br>
2017-09-29 18:47:12 -0300

    feat: Add specific attribute routing

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -12,7 +12,26 @@ class DiseasesController < ApplicationController
     @disease = Disease.where("lower(name) LIKE?", "%#{permitted_params[:disease].downcase}%").first
   end
 
+  def show_attr
+    if valid?(permitted_params[:attribute].downcase)
+      @disease_attr = Disease.where("lower(name) LIKE?", "%#{permitted_params[:disease].downcase}%").select("id, #{permitted_params[:attribute].downcase}").first
+    else
+      render json: {} , status: :not_found
+    end
+  end
+
   def permitted_params
-    params.permit(:disease, :data_source)
+    params.permit(:disease, :data_source, :attribute)
+  end
+
+  private
+
+  def valid?(column)
+    if Disease.column_names.include?(column) or ["created_at", "update_at"].include?(column)
+      attr = true
+    else
+      attr = false
+    end
+    attr
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root :to => 'diseases#index'
   resources :diseases, param: :disease
 
+  get "/diseases/:disease/:attribute" => 'diseases#show_attr'
+
   # This handles 404 Error, 500 Error, and 422 Error
   get "/404" => "application#not_found"
   get "/500" => "application#exception"
