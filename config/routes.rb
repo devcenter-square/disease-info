0d5dabb5520d0c34997cab1f65a85210fba4c1da
Sunday Adefila <adefilaedward@gmail.com>
2017-10-15 22:49:13 +0100

    add endpoint to set/unset the is_active property of a disease, completes #39, fully fixes #27

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => 'diseases#index'
   resources :diseases, param: :disease
+  put '/diseases/:disease/set_active_status', to: 'diseases#set_active_status'
 
   # This handles 404 Error, 500 Error, and 422 Error
   get "/404" => "application#not_found"
--- spec/controllers/diseases_controller_spec.rb
+++ spec/controllers/diseases_controller_spec.rb
@@ -49,4 +49,23 @@ describe DiseasesController, type: :controller do
     end
   end
 
+  describe "PUT#set_active_status" do
+    it "should update disease to non-active when param is set to false or zero or empty" do
+      [false, nil].each do |value|
+        # disease is_active is true by default
+        disease = create(:disease)
+        put :set_active_status, disease: disease.name, is_active: value
+        expect(disease.reload.is_active).to eq false
+      end
+    end
+
+    it "should update disease to active when given a truthy value" do
+      [true, 'true'].each do |value|
+        disease = create(:disease, is_active: false)
+        put :set_active_status, disease: disease.name, is_active: value
+        expect(disease.reload.is_active).to eq true
+      end
+    end
+  end
+
 end
